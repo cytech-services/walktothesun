a3e8c8a999c872187281ec494ebbbd42e46cbaf1
Christopher Graham <chgraham@springfieldclinic.com>
2023-10-30 10:58:10 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+	<div class="flex flex-col justify-center items-center">
+		<img class="w-96 h-96" alt="Walk To The Sun logo" src="./assets/walktothesun_logo.jpg" />
+		<h1 class="text-3xl font-bold text-wtts">
+			Walk To The Sun
+		</h1>
+	</div>
+</template>
+
+<style scoped></style>
